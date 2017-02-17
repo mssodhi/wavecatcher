c584eb1be645bf5c308a6131b365de2244998b90
Manu SOdhi <mssodhi@ucdavis.edu>
2017-02-17 03:43:40 -0800

    wip

--- src/songs/songs-model.js
+++ src/songs/songs-model.js
@@ -7,13 +7,14 @@ let model = {
       trackId: data.trackId,
       date: new Date()
     });
-    //
+
+
     // db.mysqlDB.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     //   if (error) throw error;
     //   console.log('The solution is: ', results[0].solution);
     // });
 
-    return true;
+    return db.collection('songs').findOne({name: 'manu'});
   }
 };
 
--- src/songs/songs-routes.js
+++ src/songs/songs-routes.js
@@ -3,7 +3,6 @@ let router = require('express').Router();
 let model = require('./songs-model');
 
 router.post('/add', function(req, res, next) {
-  console.log(req.body);
   model.add(req.body).then(results => res.send(results), next);
 });
 
