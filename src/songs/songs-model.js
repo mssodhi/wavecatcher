a48067f6575626c8d69e97a6ed08a342162f661e
Manu SOdhi <mssodhi@ucdavis.edu>
2017-02-17 19:17:24 -0800

    wip

--- src/songs/songs-model.js
+++ src/songs/songs-model.js
@@ -3,18 +3,19 @@ let db = require('./../shared/db');
 let model = {
   add: function (data) {
 
-    db.collection('songs').insertOne({
-      userId: data.userId,
-      track: data.track,
-      date: new Date()
-    });
-
     // var query = 'INSERT into wavecatcher.logs (`userId`, `trackId`) values (\''+ data.userId + '\', \'' + data.trackId + '\');';
     // db.mysqlDB.query(query, function (error, results, fields) {
     //   if (error) throw error;
     // });
+    return new Promise(function (resolve, reject){
+      db.collection('songs').insertOne({
+        userId: data.userId,
+        track: data.track,
+        date: new Date()
+      });
+      resolve({});
+    });
 
-    return db.collection('songs').find();
   }
 };
 
