03d6f09168beab299f116aef1191115f10de8ce5
Manu SOdhi <mssodhi@ucdavis.edu>
2017-04-06 19:13:06 -0700

    fetching questions routes

--- src/intPrep/intprep-model.js
+++ src/intPrep/intprep-model.js
@@ -1,4 +1,5 @@
 const courses = require('./data/courses.json');
+const questions = require('./data/questions.json');
 
 let model = {
   getCourses: function (args) {
@@ -9,7 +10,18 @@ let model = {
         resolve(courses);
       }
     });
+  },
+
+  getQuestionsForCourse: function(id) {
+    return new Promise(function (resolve, reject){
+      if(id) {
+        resolve(questions.filter(question => question.course.id == id));
+      } else {
+        reject()
+      }
+    });
   }
+
 };
 
 module.exports = model;
--- src/intPrep/intprep-routes.js
+++ src/intPrep/intprep-routes.js
@@ -6,4 +6,8 @@ router.get('/courses', function(req, res, next) {
   model.getCourses().then(results => res.send(results), next);
 });
 
+router.get('/course/:id/questions', function(req, res, next) {
+  model.getQuestionsForCourse(req.params.id).then(results => res.send(results), next);
+});
+
 module.exports = router;
